6ff4ae0f3ad95be3ae13dbadf8d0e099b09cc420
haroldks <kiossouharold@gmail.com>
2025-05-30 13:50:38 +0200

    Refractoring

--- src/algorithms/mod.rs
+++ src/algorithms/mod.rs
@@ -2,13 +2,12 @@ use crate::cover::Cover;
 use crate::tree::Tree;
 
 mod common;
+mod optimal;
 
 pub trait TreeSearchAlgorithm {
     
     fn fit(&mut self, cover: &mut Cover);
     
     fn tree(&self) -> &Tree;
-    
-    
     
 }
