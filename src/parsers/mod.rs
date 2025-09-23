6f48e0a42955deefc08dfbd9db30bdac04189d54
haroldks <kiossouharold@gmail.com>
2025-09-23 11:17:11 +0200

    Modify parsers

--- src/parsers/mod.rs
+++ src/parsers/mod.rs
@@ -1,34 +1,35 @@
-use crate::searches::{
-    BranchingStrategy, CacheInitStrategy, CacheType, D2Objective, LowerBoundStrategy,
-    SearchHeuristic, SearchStrategy, Specialization,
-};
+pub mod examples;
+
+use crate::algorithms::common::types::{BranchingPolicy, OptimalDepth2Policy, LowerBoundPolicy, SearchStrategy, CacheType, SearchHeuristic};
 use clap::{arg, Parser, Subcommand};
 use std::path::PathBuf;
 
+
 #[derive(Debug, Parser)]
 #[clap(name = "dt-trees", version, author, about)]
-pub struct App {
+pub struct MainApp {
     /// Dataset input file path
     #[clap(short, long, value_parser)]
-    pub(crate) input: PathBuf,
+    pub input: PathBuf,
 
     #[clap(subcommand)]
-    pub(crate) command: ArgCommand,
+    pub command: ArgCommand,
 
     /// Printing Statistics and Constraints
     #[arg(long, default_value_t = false)]
-    pub(crate) print_stats: bool,
+    pub print_stats: bool,
 
     /// Printing Tree
     #[arg(long, default_value_t = false)]
-    pub(crate) print_tree: bool,
+    pub print_tree: bool,
+
 }
 
 #[derive(Debug, Subcommand)]
-pub(crate) enum ArgCommand {
+pub enum ArgCommand {
     /// DL8.5 Optimal search Algorithm with no depth limit and classification error as criterion.
     /// TODO : More arguments will be added to support LDS.
-    dl85 {
+    DL85 {
         /// Minimum support
         #[arg(short, long, default_value_t = 1)]
         support: usize,
@@ -39,34 +40,26 @@ pub(crate) enum ArgCommand {
 
         /// Sorting Features based on heuristic only at the root (true) or at each node
         #[arg(long, default_value_t = true)]
-        sorting_once: bool,
+        always_sort: bool,
 
         /// Use Murtree Specialization Algorithm
-        #[arg(long, value_enum, default_value_t = Specialization::None_)]
-        specialization: Specialization,
+        #[arg(long, value_enum, default_value_t = OptimalDepth2Policy::Enabled)]
+        depth2_policy: OptimalDepth2Policy,
 
         /// Lower bound heuristic strategy
-        #[arg(long="lb", value_enum, default_value_t = LowerBoundStrategy::None_)]
-        lower_bound_heuristic: LowerBoundStrategy,
+        #[arg(long="lb", value_enum, default_value_t = LowerBoundPolicy::Disabled)]
+        lower_bound_policy: LowerBoundPolicy ,
 
         /// Branching type
-        #[arg(short, long, value_enum, default_value_t = BranchingStrategy::None_)]
-        branching: BranchingStrategy,
+        #[arg(short, long, value_enum, default_value_t = BranchingPolicy::Default)]
+        branching_policy: BranchingPolicy,
 
         #[arg(long="cache", value_enum, default_value_t = CacheType::Trie)]
         cache_type: CacheType,
 
-        /// Cache init size
-        /// Represents the reserved starting size of the cache
-        #[arg(long, default_value_t = 0)]
-        cache_init_size: usize,
-
-        /// Cache Initialization strategy
-        #[arg(long, value_enum, default_value_t = CacheInitStrategy::None_)]
-        init_strategy: CacheInitStrategy,
 
         /// Sorting heuristic
-        #[arg(long, value_enum, default_value_t = SearchHeuristic::None_)]
+        #[arg(long, value_enum, default_value_t = SearchHeuristic::NoHeuristic)]
         heuristic: SearchHeuristic,
 
         /// Tree error initial upper bound
@@ -75,11 +68,15 @@ pub(crate) enum ArgCommand {
 
         /// Maximum time allowed to the search
         #[clap(long, short)]
-        timeout: Option<usize>,
+        timeout: Option<f64>,
+
+        /// Printing Config
+        #[arg(long, default_value_t = false)]
+        print_config: bool,
     },
 
     /// Optimal depth 2 algorithms using Error or Information as criterion
-    d2_odt {
+    D2 {
         /// Minimum support
         #[arg(short, long, default_value_t = 1)]
         support: usize,
@@ -90,12 +87,12 @@ pub(crate) enum ArgCommand {
         depth: usize,
 
         /// Objective to optimise. Error or Information Gain
-        #[arg(short, long, value_enum, default_value_t = D2Objective::Error)]
-        objective: D2Objective,
+        #[arg(short, long, value_enum, default_value_t = SearchStrategy::Depth2ErrorMinimizer)]
+        objective: SearchStrategy,
     },
 
-    /// Less greedy decision tree approach usind misclassification or information gain tree as sliding window
-    lgdt {
+    /// Less greedy decision tree approach using misclassification or information gain tree as sliding window
+    LGDT {
         /// Minimum support
         #[arg(short, long, default_value_t = 1)]
         support: usize,
@@ -105,7 +102,11 @@ pub(crate) enum ArgCommand {
         depth: usize,
 
         /// Objective function inside
-        #[arg(short, long, value_enum, default_value_t = D2Objective::Error)]
-        objective: D2Objective,
+        #[arg(short, long, value_enum, default_value_t = SearchStrategy::Depth2ErrorMinimizer)]
+        objective: SearchStrategy,
+
+        /// Printing Config
+        #[arg(long, default_value_t = false)]
+        print_config: bool,
     },
 }
