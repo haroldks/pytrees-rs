8918338f1b441da61bea9b5f923fca0ee517206a
haroldks <kiossouharold@gmail.com>
2025-09-25 09:57:42 +0200

    Publishing v1.0.0

--- pytrees-rs/setup.py
+++ pytrees-rs/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from setuptools_rust import Binding, RustExtension
 
 setup(
-    version="0.0.1",
+    version="1.0.0",
     rust_extensions=[RustExtension("pytreesrs", binding=Binding.PyO3)],
     packages=find_packages(),
     # rust extensions are not zip safe, just like C-extensions.
